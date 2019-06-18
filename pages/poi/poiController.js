be87695b96b83a0163e105bed9bac72fe1c16cbc
nofartu <nofartu@post.bgu.ac.il>
2019-06-18 20:14:52 +0300

    added the saved and orgenaized bettter

--- pages/poi/poiController.js
+++ pages/poi/poiController.js
@@ -1,10 +1,12 @@
 angular.module("myApp")
     .controller("poiController", function ($scope, $http, $window, $q) {
         $scope.poiShow = [];
+        $scope.poiSaved = [];
         $scope.init = function () {
             let stringArr = "";
-            let arr=[];
+            let arr = [];
             if ($window.sessionStorage.getItem("favoritesname") !== null) {
+                $scope.poiShow.found=true;
                 stringArr = $window.sessionStorage.getItem("favoritesname");
                 arr = stringArr.split(',');
                 $scope.defer = $q.defer();
@@ -14,17 +16,17 @@ angular.module("myApp")
                     for (i = 0; i < arr.length; i++) {
                         for (j = 0; j < $scope.pois.length; j++) {
                             if (arr[i] === $scope.pois[j].poiname) {
-                                $scope.pois[j].favorites=false;
+                                $scope.pois[j].favorites = false;
                                 tmp.push($scope.pois[j]);
-
                             }
                         }
                     }
                     $scope.poiShow = tmp;
+                    $scope.showAll();
                 });
             }
             else {
-                alert("you dont have saved items");
+                $scope.poiShow.found=false;
             }
 
 
@@ -69,45 +71,55 @@ angular.module("myApp")
             }
             $http(req).then(function (response) {
                 console.log(response.data);
-                if(response.data===false){
+                if (response.data === false) {
 
-                }
-                else {
-                    selected.favorites=true;
-                    $scope.message="Your POI is saved";
-                    $scope.showMessage=true;
-                    let tmp1=new Array();
-                    let tmpString="";
-                    for(k=0;k<$scope.poiShow.length;k++){
-                        if($scope.poiShow[k].poiname!==selected.poiname){
-                            tmp1.push($scope.poiShow[k])
+                } else {
+                    selected.favorites = true;
+                    $scope.message = "Your POI is saved";
+                    $scope.showMessage = true;
+                    let tmp1 = new Array();
+                    let tmpString = "";
+                    let tmpNums="";
+                    let num=0;
+                    for (k = 0; k < $scope.poiShow.length; k++) {
+                        if ($scope.poiShow[k].poiname !== selected.poiname) {
+                            tmp1.push($scope.poiShow[k]);
+                            num++;
+                        }
+                        else{
+                            $scope.poiShow[k].favorites=false;
+                            $scope.poiSaved.push($scope.poiShow[k]);
                         }
                     }
-                    $scope.poiShow=tmp1;
-                    for(k=0;k<$scope.poiShow.length;k++){
-                       if(k!=0){
-                           tmpString=tmpString+","+$scope.poiShow[k].poiname;
-                       }else {
-                           tmpString=$scope.poiShow[k].poiname;
-                       }
+                    $scope.poiShow = tmp1;
+                    for (k = 0; k < $scope.poiShow.length; k++) {
+                        if (k !== 0) {
+                            tmpString = tmpString + "," + $scope.poiShow[k].poiname;
+                            tmpNums = tmpNums + "," + $scope.poiShow[k].poiId;
+
+                        } else {
+                            tmpString = $scope.poiShow[k].poiname;
+                            tmpNums = $scope.poiShow[k].poiId;
+
+                        }
                     }
-                    $window.sessionStorage.setItem("num",$scope.nember-1);
-
+                    $window.sessionStorage.setItem("num", num);
+                    $window.sessionStorage.setItem("favoritesname", tmpString);
+                    $window.sessionStorage.setItem("favorites", tmpNums);
                 }
-
             });
         }
 
-        $scope.AddReview=function(selected){
-            if($scope.rank!=null){
+        $scope.AddReview = function (selected) {
+            if ($scope.rank != null) {
                 $scope.AddRank(selected);
             }
-            if($scope.review!=null){
+            if ($scope.review != null) {
                 $scope.AddReviewDB(selected);
             }
         }
 
-        $scope.AddRank=function (selected) {
+        $scope.AddRank = function (selected) {
             let req = {
                 method: 'POST',
                 url: 'http://localhost:3000/saveRankPoi',
@@ -118,19 +130,16 @@ angular.module("myApp")
             }
             $http(req).then(function (response) {
                 console.log(response.data);
-                if(response.data===false){
+                if (response.data === false) {
+
+                } else {
+                    alert("your rank is saved");
 
                 }
-                else {
-          alert("your rank is saved");
-
-                }
-
             });
-
         }
 
-        $scope.AddReviewDB=function (selected) {
+        $scope.AddReviewDB = function (selected) {
             let req = {
                 method: 'POST',
                 url: 'http://localhost:3000/saveReviewPoi',
@@ -141,15 +150,103 @@ angular.module("myApp")
             }
             $http(req).then(function (response) {
                 console.log(response.data);
-                if(response.data===false){
+                if (response.data === false) {
 
-                }
-                else {
+                } else {
                     alert("your rank is saved");
                 }
-
             });
+        }
+        
+        $scope.showAll=function () {
+            let req = {
+                method: 'GET',
+                url: 'http://localhost:3000/private/getAllPOIBu',
+                headers: {
+                    'x-auth-token': $window.sessionStorage.getItem("token")
+                }
+            }
+            $http(req).then(function (response) {
+                console.log(response.data);
+                if (response.data === false) {
+                    $scope.poiSaved.found=false;
+                } else {
 
+                    $scope.poiSaved=response.data;
+                    $scope.poiSaved.found=true;
+                    // selected.favorites = true;
+                    // $scope.message = "Your POI is saved";
+                    // $scope.showMessage = true;
+                    // let tmp1 = new Array();
+                    // let tmpString = "";
+                    // let num=0;
+                    //
+                    //
+                    // for (k = 0; k < $scope.poiShow.length; k++) {
+                    //     if ($scope.poiShow[k].poiname !== selected.poiname) {
+                    //         tmp1.push($scope.poiShow[k]);
+                    //         num++;
+                    //     }
+                    // }
+                    // $scope.poiShow = tmp1;
+                    // for (k = 0; k < $scope.poiShow.length; k++) {
+                    //     if (k !== 0) {
+                    //         tmpString = tmpString + "," + $scope.poiShow[k].poiname;
+                    //     } else {
+                    //         tmpString = $scope.poiShow[k].poiname;
+                    //     }
+                    // }
+                    //
+                    // $window.sessionStorage.setItem("num", num);
+                    // $window.sessionStorage.setItem("favoritesname", $scope.poiShow);
+                }
+            });
+        }
+
+        $scope.deleteFromDB=function (selected) {
+            let req = {
+                method: 'GET',
+                url: 'http://localhost:3000/private/deleteUserPoi',
+                headers: {
+                    'x-auth-token': $window.sessionStorage.getItem("token")
+                },
+                params: {
+                    poiId: selected.poiId
+                }
+            }
+            $http(req).then(function (response) {
+                console.log(response.data);
+                // if (response.data === false) {
+                //     $scope.poiSaved.found=false;
+                // } else {
+                //     $scope.poiSaved=response.data;
+                //     $scope.poiSaved.found=true;
+                //     for (k = 0; k < $scope.poiShow.length; k++) {
+                //         if ($scope.poiShow[k].poiname !== selected.poiname) {
+                //             tmp1.push($scope.poiShow[k]);
+                //             num++;
+                //         }
+                //         else{
+                //             $scope.poiShow[k].favorites=false;
+                //             $scope.poiSaved.push($scope.poiShow[k]);
+                //         }
+                //     }
+                //     $scope.poiShow = tmp1;
+                //     for (k = 0; k < $scope.poiShow.length; k++) {
+                //         if (k !== 0) {
+                //             tmpString = tmpString + "," + $scope.poiShow[k].poiname;
+                //             tmpNums = tmpNums + "," + $scope.poiShow[k].poiId;
+                //
+                //         } else {
+                //             tmpString = $scope.poiShow[k].poiname;
+                //             tmpNums = $scope.poiShow[k].poiId;
+                //         }
+                //     }
+                //     $window.sessionStorage.setItem("num", num);
+                //     $window.sessionStorage.setItem("favoritesname", tmpString);
+                //     $window.sessionStorage.setItem("favorites", tmpNums);
+                // }
+            });
         }
 
     });
